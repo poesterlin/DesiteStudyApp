cc42449d8ae3dc18cb350f160c8ca28abe86b9fa
Unknown <philip.oesterlin@gmail.com>
2019-06-29 23:57:27 +0200

    clean up chart

--- src/Charts.svelte
+++ src/Charts.svelte
@@ -14,52 +14,90 @@
 
   function create(data) {
     const ctx = document.getElementById("chart");
+    const ctx2 = document.getElementById("chart2");
 
     const modifier = getType;
 
     const groups = group(data, modifier);
 
-    const c = new Chart(ctx, {
-      type: "line",
-      data: {
-        labels: groups.map(g => modifier(g[0])),
-        datasets: [
+    const questionGroups = groups.map(g =>
+      group(g, el => el.data.task.question)
+    );
+
+    const options = {
+      scales: {
+        yAxes: [
           {
-            label: "time by visualization type",
-            data: groups.map(g =>
-              avg(g.filter(d => d.flag).map(d => d.data.timing))
-            )
-          },
+            ticks: {
+              beginAtZero: true
+            }
+          }
+        ],
+        xAxes: [
           {
-            label: "wrong by visualization type",
-            data: groups.map(g =>
-              sum(g.filter(d => !d.flag).map(f => (f ? 1 : 0)))
-            ),
-            backgroundColor: "red",
-            borderColor: "red"
+            ticks: {
+              fontSize: 9
+            }
           }
         ]
-      },
-      options: {
-        scales: {
-          yAxes: [
-            {
-              ticks: {
-                beginAtZero: true
-              }
-            }
-          ]
-        }
       }
+    };
+
+    new Chart(ctx, {
+      type: "bar",
+      data: {
+        labels: questionGroups[0].map((vis, i) => getQuestion(vis[i])),
+        datasets: questionGroups.map(questions => {
+          return {
+            label: getType(questions[0][0]),
+            data: questions.map(q =>
+              avg(q.filter(f => f.flag).map(f => f.data.timing))
+            ),
+            backgroundColor: getColor(questions[0][0]),
+            borderColor: getColor(questions[0][0])
+          };
+        })
+      },
+      options
     });
 
-    console.log(c.data.datasets);
+    const c = new Chart(ctx2, {
+      type: "bar",
+      data: {
+        labels: questionGroups[0].map((vis, i) => getQuestion(vis[i])),
+        datasets: questionGroups.map(questions => {
+          return {
+            label: getType(questions[0][0]),
+            data: questions.map(q => sum(q.filter(f => !f.flag).map(() => 1))),
+            backgroundColor: getColor(questions[0][0]),
+            borderColor: getColor(questions[0][0])
+          };
+        })
+      },
+      options
+    });
   }
 
   function getType(el) {
     const img = el.data.task.image;
     const f = img.indexOf(" (");
     return img.substr(7, f - 7);
+  }
+
+  function getQuestion(el) {
+    return el.data.task.question;
+  }
+
+  function getColor(el) {
+    const type = getType(el);
+    switch (type) {
+      case "Desite":
+        return "lightblue";
+      case "Maps":
+        return "green";
+      case "DB":
+        return "red";
+    }
   }
 
   function group(data, fn) {
@@ -75,11 +113,17 @@
     return sum(data) / data.length;
   }
   function sum(data) {
+    if (!Array.isArray(data) || data.length === 0 || Array.isArray(data[0])) {
+      return 0;
+    }
     return data.reduce((a, b) => a + b, 0);
   }
 </script>
 
 <style>
+  :global(body) {
+    overflow: auto;
+  }
   div {
     display: block;
     height: 80vh;
@@ -87,6 +131,11 @@
   }
 </style>
 
+Timing:
 <div class="chart-container">
   <canvas id="chart" />
+</div>
+Errors:
+<div class="chart-container">
+  <canvas id="chart2" />
 </div>
